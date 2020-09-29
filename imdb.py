2f46cb41e1e390b57bf77e8f13ee75e441618fa0
Mazzi <lor.bellino@gmail.com>
2020-09-29 12:26:34 +0200

    :)

--- imdb.py
+++ imdb.py
@@ -59,7 +59,7 @@ x_val = train_data[:10000]
 x_train = train_data[10000:]
 
 y_val = train_labels[:10000]
-y_train = train_labels[:10000]
+y_train = train_labels[10000:]
 
 #batch size = quante reviews vengono caricate ogni volta
 fitModel = model.fit(x_train, y_train, epochs = 40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
